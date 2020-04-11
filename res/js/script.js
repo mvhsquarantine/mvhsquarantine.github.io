2c47f4a0a2acc7c4c0a35d8faa2c39fb3678f78e
mvhsquarantine <63522638+mvhsquarantine@users.noreply.github.com>
2020-04-11 16:17:32 -0700

    Add files via upload

---
+++ res/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log("Page Loaded");
+});
+
+$(".covid-button").click(function() {
+    window.open('https://www.sccgov.org/sites/phd/DiseaseInformation/novel-coronavirus/Pages/home.aspx','_blank');
+})
